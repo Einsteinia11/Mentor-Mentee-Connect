26571d6f046345b1d2c85199b82306cbff5768fc
Einsteinia <informationviakate2004@gmail.com>
2023-06-28 16:12:14 +0530

    commit

---
+++ connect/src/comp2.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function comp2() {
+  return (
+    <div>This is component 2</div>
+  )
+}
+
+export default comp2
+//rfce
+
+
+
+
+
---
+++ connect/src/component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function component() {
+  return (
+    <div>TThis is component 1</div>
+  )
+}
+
+export default component
+//rfce
